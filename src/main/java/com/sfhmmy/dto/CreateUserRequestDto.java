c48625e2eea4c8e2e0092fb8c88fc02c124dd8d6
Alexis Panousis <apanousis@gmail.com>
2022-04-16 12:42:20 +0300

    (feature) adds create user method (without implementation)

---
+++ src/main/java/com/sfhmmy/dto/CreateUserRequestDto.java
@@ -0,0 +1,29 @@
+package com.sfhmmy.dto;
+
+public class CreateUserRequestDto {
+    private String firstName, lastName, email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
